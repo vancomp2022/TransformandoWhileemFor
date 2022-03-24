01ee66d700a878c683769138c71ae79beb7aae29
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-23 23:12:16 -0300

    Adicionar arquivos de projeto.

---
+++ TransformandoWhileemFor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(String[] args)
+    {
+        
+        for (int contador = 1; contador <= 10; contador++)
+        {
+            Console.WriteLine(contador);
+  
+        }
+    }
+}
